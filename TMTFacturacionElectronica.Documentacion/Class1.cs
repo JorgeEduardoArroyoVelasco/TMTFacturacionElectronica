b2515c4f120b3af7ba2997a7628a6fa6faf9f715
Eduardo Arroyo <gorjearollo@hotmail.com>
2019-06-11 11:46:05 -0500

    Arquitectura inicial solución TMT Facturación

---
+++ TMTFacturacionElectronica.Documentacion/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMTFacturacionElectronica.Documentacion
+{
+    public class Class1
+    {
+    }
+}
---
+++ TMTFacturacionElectronica.Entities/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMTFacturacionElectronica.Entities
+{
+    public class Class1
+    {
+    }
+}
